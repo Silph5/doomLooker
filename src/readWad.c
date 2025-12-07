2d64d585cf8b84d9a53728cd3bc0b807c82e9f76
Silph5 <tj.adamson@sky.com>
2025-12-07 21:21:40 +0000

    find offset to given map marker

--- src/readWad.c
+++ src/readWad.c
@@ -16,12 +16,36 @@ typedef struct {
     int directoryOffset;
 } header;
 
+typedef struct {
+    int lumpOffs;
+    int lumpSize;
+    char lumpName[8];
+} directoryEntry;
+
 void readHeader(FILE* wad, header* head) {
     fread(&head->ident, sizeof(char), 4, wad);
     head->ident[4] = '\0';
 
     fread(&head->lumpsNum, sizeof(int), 1, wad);
     fread(&head->directoryOffset, sizeof(int), 1, wad);
+}
+
+int findTargetMapLump(FILE* wad, directoryEntry* entry, int dirOffset, int lumpsNum, char* mapName) {
+    fseek(wad, dirOffset, 0);
+
+    for (int l = 0; l < lumpsNum; l++) {
+        fread(&entry->lumpOffs, sizeof(int), 1, wad);
+        fread(&entry->lumpSize, sizeof(int), 1, wad);
+        fread(&entry->lumpName, sizeof(char), 8, wad);
+
+        if (strcmp(entry->lumpName, mapName) == 0) {
+            printf("lumpOffset: %i\n", entry->lumpOffs);
+            printf("lumpSize: %i\n", entry->lumpSize);
+            printf("lumpName: %s\n", entry->lumpName);
+            return entry->lumpOffs;
+        }
+    }
+    return -1;
 }
 
 doomMap* readWadToMapData(const char* wadPath, char* mapName) {
@@ -34,13 +58,17 @@ doomMap* readWadToMapData(const char* wadPath, char* mapName) {
     header header;
     readHeader(wad, &header);
     if (strcmp(header.ident, "IWAD") != 0 && strcmp(header.ident, "PWAD") != 0) {
-        fprintf(stderr, "Given file is not a WAD.\n");
+        fprintf(stderr, "Incorrect file type given.\nfile ident:%s\n", header.ident);
         return NULL;
     }
 
     printf("Ident: %s\n", header.ident);
     printf("number of lumps: %i\n", header.lumpsNum);
     printf("directory offset: %i\n", header.directoryOffset);
+
+    directoryEntry entry;
+
+    int mapPtr = findTargetMapLump(wad, &entry, header.directoryOffset, header.lumpsNum, mapName);
 
     return NULL;
 }
